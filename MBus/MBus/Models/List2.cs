996e7cd366cc818645b5209fb37914b5ac86229e
Hein Andre Grønnestad <hag@haugnett.no>
2018-09-29 00:41:24 +0200

    Renamed List9 to List2, added more fields to list

---
+++ MBus/MBus/Models/List2.cs
@@ -0,0 +1,22 @@
+﻿namespace MBus.Models {
+
+    public class List2 : ListBase {
+        public string ListVersionIdentifier { get; set; }
+        public string MeterId { get; set; }
+        public string MeterType { get; set; }
+
+        public uint ActivePowerImport { get; set; }
+        public uint ActivePowerExport { get; set; }
+        public uint ReactivePowerImport { get; set; }
+        public uint ReactivePowerExport { get; set; }
+        public int CurrentL1 { get; set; }
+        public int CurrentL2 { get; set; }
+        public int CurrentL3 { get; set; }
+        public uint VoltageL1 { get; set; }
+        public uint VoltageL2 { get; set; }
+        public uint VoltageL3 { get; set; }
+
+        public double CurrentL1Double => CurrentL1 / 1000.0f;
+        public double VoltageL1Double => VoltageL1 / 10.0f;
+    }
+}
--- MBus/MBus/Models/List9.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MBus.Models {
-
-    public class List9 : ListBase {
-        public string ListVersionIdentifier { get; set; }
-        public string MeterId { get; set; }
-        public string MeterType { get; set; }
-    }
-}
