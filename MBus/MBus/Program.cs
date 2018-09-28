57ada3a05f8c2098df134fcf8cce0dc88f0a04a7
Hein Andre Grønnestad <hag@haugnett.no>
2018-09-29 00:45:53 +0200

    Added decode support for all remaining data for single phase meters, changed GetPacket to be able to read from file dumps

--- MBus/MBus/Program.cs
+++ MBus/MBus/Program.cs
@@ -1,6 +1,7 @@
-using MBus.Models;
+﻿using MBus.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,10 @@ namespace MBus {
             Sp.Open();
 
             while (true) {
-                var packet = GetPacket();
+                //var packet = GetPacket(Sp.BaseStream);
+
+                var fs = File.OpenRead(@"RawPacketDumps\List3SinglePhase.dat");
+                var packet = GetPacket(fs);
 
                 switch (packet) {
                     case List1 list:
@@ -30,12 +34,45 @@ namespace MBus {
                         Console.WriteLine("");
                         break;
 
-                    case List9 list:
+                    case List3 list:
                         Console.WriteLine($"Received {list.Type}:");
                         Console.WriteLine($"DateTime: {list.Date}");
                         Console.WriteLine($"ListVersionIdentifier: {list.ListVersionIdentifier}");
                         Console.WriteLine($"MeterId: {list.MeterId}");
                         Console.WriteLine($"MeterType: {list.MeterType}");
+                        Console.WriteLine($"ActivePowerImport: {list.ActivePowerImport} W");
+                        Console.WriteLine($"ActivePowerExport: {list.ActivePowerExport} W");
+                        Console.WriteLine($"ReactivePowerImport: {list.ReactivePowerImport} VAr");
+                        Console.WriteLine($"ReactivePowerExport: {list.ReactivePowerExport} VAr");
+                        Console.WriteLine($"CurrentL1: {list.CurrentL1}");
+                        Console.WriteLine($"VoltageL1: {list.VoltageL1}");
+                        Console.WriteLine($"CurrentL1Double: {list.CurrentL1Double} A");
+                        Console.WriteLine($"VoltageL1Double: {list.VoltageL1Double} V");
+                        Console.WriteLine($"CumulativeActiveImportEnergy: {list.CumulativeActiveImportEnergy}");
+                        Console.WriteLine($"CumulativeActiveExportEnergy: {list.CumulativeActiveExportEnergy}");
+                        Console.WriteLine($"CumulativeReactiveImportEnergy: {list.CumulativeReactiveImportEnergy}");
+                        Console.WriteLine($"CumulativeReactiveExportEnergy: {list.CumulativeReactiveExportEnergy}");
+                        Console.WriteLine($"CumulativeActiveImportEnergyDouble: {list.CumulativeActiveImportEnergyDouble} kWh");
+                        Console.WriteLine($"CumulativeActiveExportEnergyDouble: {list.CumulativeActiveExportEnergyDouble} kWh");
+                        Console.WriteLine($"CumulativeReactiveImportEnergyDouble: {list.CumulativeReactiveImportEnergyDouble} KVArh");
+                        Console.WriteLine($"CumulativeReactiveExportEnergyDouble: {list.CumulativeReactiveExportEnergyDouble} KVArh");
+                        Console.WriteLine("");
+                        break;
+
+                    case List2 list:
+                        Console.WriteLine($"Received {list.Type}:");
+                        Console.WriteLine($"DateTime: {list.Date}");
+                        Console.WriteLine($"ListVersionIdentifier: {list.ListVersionIdentifier}");
+                        Console.WriteLine($"MeterId: {list.MeterId}");
+                        Console.WriteLine($"MeterType: {list.MeterType}");
+                        Console.WriteLine($"ActivePowerImport: {list.ActivePowerImport} W");
+                        Console.WriteLine($"ActivePowerExport: {list.ActivePowerExport} W");
+                        Console.WriteLine($"ReactivePowerImport: {list.ReactivePowerImport} VAr");
+                        Console.WriteLine($"ReactivePowerExport: {list.ReactivePowerExport} VAr");
+                        Console.WriteLine($"CurrentL1: {list.CurrentL1}");
+                        Console.WriteLine($"VoltageL1: {list.VoltageL1}");
+                        Console.WriteLine($"CurrentL1Double: {list.CurrentL1Double} A");
+                        Console.WriteLine($"VoltageL1Double: {list.VoltageL1Double} V");
                         Console.WriteLine("");
                         break;
                 }
@@ -43,19 +80,19 @@ namespace MBus {
 
         }
 
-        private static ListBase GetPacket() {
+        private static ListBase GetPacket(Stream stream) {
             while (true) {
                 var packet = new List<byte>();
 
-                var b = Sp.ReadByte();
+                var b = stream.ReadByte();
                 if (b != 0x7E) continue;
-                if (Sp.ReadByte() == 0x7E) continue;
+                if (stream.ReadByte() == 0x7E) continue;
 
-                var packetLength = (byte) Sp.ReadByte();
+                var packetLength = (byte) stream.ReadByte();
                 packet.AddRange(new byte[] { 0x7E, 0xA0, packetLength });
 
                 while (packet.Count < packetLength + 2) {
-                    packet.Add((byte) Sp.ReadByte());
+                    packet.Add((byte) stream.ReadByte());
                 }
 
                 var type = (ListType) packet[32];
@@ -64,8 +101,11 @@ namespace MBus {
                     case ListType.List1:
                         return DecodeList1Packet(packet);
 
-                    case ListType.List9:
-                        return DecodeList9Packet(packet);
+                    case ListType.List2SinglePhase:
+                        return DecodeList2SinglePhasePacket(packet);
+
+                    case ListType.List3SinglePhase:
+                        return DecodeList3SinglePhasePacket(packet);
 
                     default:
                         Console.WriteLine("Found unknown list:");
@@ -89,17 +129,50 @@ namespace MBus {
             };
         }
 
-        private static ListBase DecodeList9Packet(List<byte> data) {
+        private static ListBase DecodeList2SinglePhasePacket(List<byte> data) {
             var year = BitConverter.ToUInt16(data.Skip(19).Take(2).Reverse().ToArray(), 0);
             var date = new DateTime(year, data[21], data[22], data[24], data[25], data[26]);
 
-            return new List9() {
-                Type = ListType.List9,
+            return new List2() {
+                Type = ListType.List2SinglePhase,
                 RawData = data,
                 Date = date,
                 ListVersionIdentifier = Encoding.ASCII.GetString(data.Skip(35).Take(data[34]).ToArray()),
                 MeterId = Encoding.ASCII.GetString(data.Skip(44).Take(data[43]).ToArray()),
                 MeterType = Encoding.ASCII.GetString(data.Skip(62).Take(data[61]).ToArray()),
+
+                ActivePowerImport = BitConverter.ToUInt32(data.Skip(71).Take(4).Reverse().ToArray(), 0),
+                ActivePowerExport = BitConverter.ToUInt32(data.Skip(76).Take(4).Reverse().ToArray(), 0),
+                ReactivePowerImport = BitConverter.ToUInt32(data.Skip(81).Take(4).Reverse().ToArray(), 0),
+                ReactivePowerExport = BitConverter.ToUInt32(data.Skip(86).Take(4).Reverse().ToArray(), 0),
+                CurrentL1 = BitConverter.ToInt32(data.Skip(91).Take(4).Reverse().ToArray(), 0),
+                VoltageL1 = BitConverter.ToUInt32(data.Skip(96).Take(4).Reverse().ToArray(), 0),
+            };
+        }
+
+        private static ListBase DecodeList3SinglePhasePacket(List<byte> data) {
+            var year = BitConverter.ToUInt16(data.Skip(19).Take(2).Reverse().ToArray(), 0);
+            var date = new DateTime(year, data[21], data[22], data[24], data[25], data[26]);
+
+            return new List3() {
+                Type = ListType.List3SinglePhase,
+                RawData = data,
+                Date = date,
+                ListVersionIdentifier = Encoding.ASCII.GetString(data.Skip(35).Take(data[34]).ToArray()),
+                MeterId = Encoding.ASCII.GetString(data.Skip(44).Take(data[43]).ToArray()),
+                MeterType = Encoding.ASCII.GetString(data.Skip(62).Take(data[61]).ToArray()),
+
+                ActivePowerImport = BitConverter.ToUInt32(data.Skip(71).Take(4).Reverse().ToArray(), 0),
+                ActivePowerExport = BitConverter.ToUInt32(data.Skip(76).Take(4).Reverse().ToArray(), 0),
+                ReactivePowerImport = BitConverter.ToUInt32(data.Skip(81).Take(4).Reverse().ToArray(), 0),
+                ReactivePowerExport = BitConverter.ToUInt32(data.Skip(86).Take(4).Reverse().ToArray(), 0),
+                CurrentL1 = BitConverter.ToInt32(data.Skip(91).Take(4).Reverse().ToArray(), 0),
+                VoltageL1 = BitConverter.ToUInt32(data.Skip(96).Take(4).Reverse().ToArray(), 0),
+
+                CumulativeActiveImportEnergy = BitConverter.ToUInt32(data.Skip(115).Take(4).Reverse().ToArray(), 0),
+                CumulativeActiveExportEnergy = BitConverter.ToUInt32(data.Skip(120).Take(4).Reverse().ToArray(), 0),
+                CumulativeReactiveImportEnergy = BitConverter.ToUInt32(data.Skip(125).Take(4).Reverse().ToArray(), 0),
+                CumulativeReactiveExportEnergy = BitConverter.ToUInt32(data.Skip(130).Take(4).Reverse().ToArray(), 0),
             };
         }
 
