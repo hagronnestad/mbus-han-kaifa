2648c110392e84be91783e112a515969ef513a5d
Hein Andre Grønnestad <hag@haugnett.no>
2018-09-29 00:44:07 +0200

    Added List3 class

---
+++ MBus/MBus/Models/List3.cs
@@ -0,0 +1,14 @@
+﻿namespace MBus.Models {
+
+    public class List3 : List2 {
+        public uint CumulativeActiveImportEnergy { get; set; }
+        public uint CumulativeActiveExportEnergy { get; set; }
+        public uint CumulativeReactiveImportEnergy { get; set; }
+        public uint CumulativeReactiveExportEnergy { get; set; }
+
+        public double CumulativeActiveImportEnergyDouble => CumulativeActiveImportEnergy / 1000.0f;
+        public double CumulativeActiveExportEnergyDouble => CumulativeActiveExportEnergy / 1000.0f;
+        public double CumulativeReactiveImportEnergyDouble => CumulativeReactiveImportEnergy / 1000.0f;
+        public double CumulativeReactiveExportEnergyDouble => CumulativeReactiveExportEnergy / 1000.0f;
+    }
+}
