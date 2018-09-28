e12e23551f81e1ea060622f407213d76735da7ba
Hein Andre Grønnestad <hag@haugnett.no>
2018-09-29 00:37:40 +0200

    Refined ListType enum based on more reverse engineering

--- MBus/MBus/Models/ListType.cs
+++ MBus/MBus/Models/ListType.cs
@@ -1,9 +1,10 @@
 ﻿namespace MBus.Models {
 
     public enum ListType {
-        List1 = 1,    // Active Power
-        List9 = 9,    // List2, but for single phase meters?
-        List2 = 13,   // 3 phase meters?
-        List3 = 18    // List2 + cumulative data (meter reading)?
+        List1 = 0x1,                // List1: Active Power (All meters)
+        List2SinglePhase = 0x9,     // List2: (Single phase meters)
+        List2 = 0x13,               // List2: (3 phase meters)
+        List3 = 0x12,               // List3: List2 + cumulative data (meter reading)
+        List3SinglePhase = 0xE      // List3: List2 + cumulative data (meter reading) (Single phase meters)
     }
 }
