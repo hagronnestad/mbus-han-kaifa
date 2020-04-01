737d410acebb06f777a1c6b5b70fec8e1e85397e
Hein Andre Grønnestad <hag@haugnett.no>
2020-04-01 21:57:11 +0200

    Fixed List2 3-phase ID

--- MBus/MBus/Models/ListType.cs
+++ MBus/MBus/Models/ListType.cs
@@ -1,10 +1,10 @@
 ﻿namespace MBus.Models {
 
     public enum ListType {
-        List1 = 0x1,                // List1: Active Power (All meters)
-        List2SinglePhase = 0x9,     // List2: (Single phase meters)
-        List2 = 0x13,               // List2: (3 phase meters)
+        List1 = 0x01,               // List1: Active Power (All meters)
+        List2SinglePhase = 0x09,    // List2: (Single phase meters)
+        List2 = 0x0D,               // List2: (3 phase meters)
         List3 = 0x12,               // List3: List2 + cumulative data (meter reading)
-        List3SinglePhase = 0xE      // List3: List2 + cumulative data (meter reading) (Single phase meters)
+        List3SinglePhase = 0x0E     // List3: List2 + cumulative data (meter reading) (Single phase meters)
     }
 }
